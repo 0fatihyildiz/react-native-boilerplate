839464e63cc14e2a15b20e5a3b9330936435a86f
Ferhat-kus <55ferhat.kus@gmail.com>
2024-07-12 09:01:49 +0300

    feat : iconify configtation completed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
-const App = () => {
+import {Iconify} from 'react-native-iconify';
+export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex flex-row h-screen items-center justify-center bg-black">
+      <Text className="text-white font-semibold text-2xl">App</Text>
+      <Iconify icon="mdi:heart" size={24} color="#900" />
     </View>
   );
-};
-
-export default App;
+}
